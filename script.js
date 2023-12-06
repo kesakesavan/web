912f4e9e92f28a7d82e5c57545098eae05309194
kesakesavan <kesa16102002@gmail.com>
2023-12-06 11:22:03 +0530

    marketing

---
+++ script.js
@@ -0,0 +1,8 @@
+<script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+
+var typed = new Typed(".input" , {
+  strings:["Marketing"],
+  typeSpeed: 70,
+  backSpeed: 65,
+  loop: true
+})
